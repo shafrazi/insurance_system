67f69f2809bbee882d7dcf8c23cbabbe2ed1b04e
shafrazi <shafrazi@live.com>
2018-12-20 01:33:00 +0530

    add customizations to forms and rectify error in saving insurance renewal along with insurance policy, add validations to models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
   has_many :insurance_policies, dependent: :destroy, inverse_of: :customer
   accepts_nested_attributes_for :insurance_policies, reject_if: :all_blank, allow_destroy: true
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/renewal.rb
+++ app/models/renewal.rb
@@ -1,3 +1,6 @@
 class Renewal < ApplicationRecord
   belongs_to :insurance_policy
+  validates :policyno, presence: true
+  validates :begin_date, presence: true
+  validates :expiry_date, presence: true
 end
