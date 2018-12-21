feb02be5de60eb74de6dd22bbc047e69ef10e03f
shafrazi <shafrazi@live.com>
2018-12-22 03:09:14 +0530

    cancelled filterrific extensions

--- app/models/insurance_policy.rb
+++ app/models/insurance_policy.rb
@@ -9,34 +9,4 @@ class InsurancePolicy < ApplicationRecord
   validates :current_expiry, presence: true
   validates :customer_id, presence: true
 
-  filterrific(
-   default_filter_params: { sorted_by: 'created_at_desc' },
-   available_filters: [
-     :sorted_by,
-     :search_query,
-     :with_current_expiry
-   ]
- )
-
- scope :search_query, lambda { |query|
-    # Filters students whose name or email matches the query
-
-  }
-  scope :sorted_by, lambda { |sort_key|
-    # Sorts students by sort_key
-
-  }
-  scope :with_current_expiry, lambda { |ref_date|
-
-  }
-
-  # This method provides select options for the `sorted_by` filter select input.
-  # It is called in the controller as part of `initialize_filterrific`.
-  def self.options_for_sorted_by
-    [
-      ['Name (a-z)', 'name_asc'],
-      ['Registration date (newest first)', 'created_at_desc'],
-      ['Registration date (oldest first)', 'created_at_asc']
-    ]
-  end
 end
