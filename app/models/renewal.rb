18c8945e4c3d9bf2d83e23bf3ac617d98f90e9a1
shafrazi <shafrazi@live.com>
2018-12-22 18:55:34 +0530

    add validation helper RenewalValidator to check if renewal expiry is later than current expiry

--- app/models/renewal.rb
+++ app/models/renewal.rb
@@ -1,5 +1,8 @@
 class Renewal < ApplicationRecord
+  include ActiveModel::Validations
   belongs_to :insurance_policy
   validates :policyno, :begin_date, :expiry_date, presence: true
-  
+
+  validates_with RenewalValidator
+
 end
