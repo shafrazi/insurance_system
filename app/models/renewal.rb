afe230d1109c0a7fc2ce13266a18d86e6cd3b2e3
shafrazi <shafrazi@live.com>
2018-12-22 17:59:00 +0530

    minor changes to validations

--- app/models/renewal.rb
+++ app/models/renewal.rb
@@ -1,6 +1,5 @@
 class Renewal < ApplicationRecord
   belongs_to :insurance_policy
-  validates :policyno, presence: true
-  validates :begin_date, presence: true
-  validates :expiry_date, presence: true
+  validates :policyno, :begin_date, :expiry_date, presence: true
+  
 end
